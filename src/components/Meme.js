07c67fd9e3f373ebf2311b083c345856d7c1e668
imskanand <the.anand3099@gmail.com>
2022-04-25 12:21:07 +0530

    logging Image url into the console

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,21 @@
 import memeImage from '../images/memeimg.png'
+import memesData from '../memesData'
 
 export default function Form() {
+  function getMeme() {
+    const memeDataArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memeDataArray.length)
+    const url = memeDataArray[randomNumber].url;
+    console.log(url)
+  }
   return (
     <main>
-      <form className='form'>
+      <div className='form'>
         <input type="text" className='form--input' placeholder='Top Text' ></input>
         <input type="text" className='form--input' placeholder='Bottom Text' ></input>
-        <button className='form--button'>Get a New Meme Image 🖼 </button>
-      </form>
-      <img src={memeImage} alt='memeImage' className='meme--image'/>
+        <button className='form--button' onClick={ getMeme }>Get a New Meme Image 🖼 </button>
+      </div>
+      <img src={ memeImage } alt='memeImage' className='meme--image' />
     </main>
   )
 }
